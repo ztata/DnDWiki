78abf9f056f786ee221d5aaa845980723e826808
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-08-29 16:23:22 -0400

    modified server.js

--- DnDWIki/server.js
+++ DnDWIki/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/dn-dwiki'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/dn-dwiki/index.html'));});
 app.listen(process.env.PORT || 8080);
